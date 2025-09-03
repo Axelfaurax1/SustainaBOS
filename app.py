827fa418824678f0182e493e7b4df82c87e3b802
Axel <axel.faurax@britoil.com.sg>
2025-09-03 16:59:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -803,7 +803,7 @@ html_template = """
         box-shadow: 0 8px 24px rgba(0,0,0,0.15);
         display: flex;
         flex-direction: column;
-        overflow: hidden;
+        /* overflow: hidden; */ to remove for moment as it's compromising input bar
         z-index: 1200;
         transition: transform .2s ease, opacity .2s ease;
       }
@@ -845,11 +845,15 @@ html_template = """
           padding: 12px;
           font-size: 0.9rem;
           color: var(--muted);
+          overflow-y: auto
       }
 
       .chat-input {
+        /* position: absolute */  /* This for not removing chat for more than 4 messages */
         display: flex;
         border-top: 1px solid var(--border);
+        padding: 8px;
+        background:#fff;
       }
 
       .chat-input input {
