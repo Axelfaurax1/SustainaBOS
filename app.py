6b472f73f5e07faf291a6a7e75cb0acb9683c414
Axel <axel.faurax@britoil.com.sg>
2025-09-08 17:34:57 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ class DeviceLog(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
 
 # To ignore warnings of openxyl, excel sheet weird format
